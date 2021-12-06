c533694408e3c3fcd88fa71ce4666c12680b9ca9
Katy <katynicole89@hotmail.com>
2021-12-05 23:57:11 -0600

    Redux Redo

---
+++ client/build/precache-manifest.f2ad26ee47c4d6f124c36daeb932819f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e1b9caaf471758e6eeac35f51996dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6932f5bf9fdfd869a6f",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "d001600c0c1a1fd0e774",
+    "url": "/static/js/2.b014156a.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.b014156a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6932f5bf9fdfd869a6f",
+    "url": "/static/js/main.dbbb628e.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
